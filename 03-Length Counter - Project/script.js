48a1967abf65589a05fea908659b51ddbc23bbf3
Halis M <elmuvakka@yandex.com>
2021-12-10 22:58:13 +0300

    js-102 3. proje bitimi

--- 03-Length Counter - Project/script.js
+++ 03-Length Counter - Project/script.js
@@ -1,7 +1,14 @@
 
 const calculateCharacter = () => {
   // Write your codes here
+let tweet = prompt("Tweetinizi giriniz");
+let tweetCount = (tweet.length);
+console.log(tweetCount);
+let tweetLeft= 150-tweetCount;
+console.log(tweetLeft);
 
+// alert("You have written" + tweetCount + "characters you have" + tweetLeft + "charecters remaining.")
+console.log("You have written" + tweetCount + "characters you have" + tweetLeft + "charecters remaining.")
 
 
 }
